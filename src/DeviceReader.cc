af1783fcce82e00f5239022cd305967bec1952e9
Solid Wires <solid.wires@gmail.com>
2024-04-04 13:41:37 -0400

    Oh well

--- src/DeviceReader.cc
+++ src/DeviceReader.cc
@@ -10,7 +10,7 @@ int main()
     //  we just want to open something and nothing more. RDWR is explicitly stated here though.
     int file = open("/dev/memory", O_RDWR | O_NONBLOCK);
 
-    const char* toWrite = "Solid";
+    const char* toWrite = "Solid"; // Tried allowing cin, but it was being painful today
 
     // Write into that device file
     ssize_t writeTest = write(file, toWrite, std::strlen(toWrite));
