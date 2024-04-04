8b1bce4258c4aa554f0aa680db72aa06013dc529
Solid Wires <solid.wires@gmail.com>
2024-04-04 01:00:14 -0400

    This had weird symbols for quotation marks...

--- modules/helloworld/hello.c
+++ modules/helloworld/hello.c
@@ -3,7 +3,7 @@
 #include <linux/kernel.h> /* printk() */
 
 MODULE_LICENSE("Dual BSD/GPL");
-MODULE_AUTHOR(“The Professor”);
+MODULE_AUTHOR("The Professor");
 
 static int hello_init(void) {
   printk("<1> Hello world!\n");
